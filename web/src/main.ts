175012529f056a2b600d886ede3877422e487284
MrCarbene <66732742+sx120609@users.noreply.github.com>
2026-08-30 01:30:13 +0800

    feat: standardize site emoji rendering

--- web/src/main.ts
+++ web/src/main.ts
@@ -20,6 +20,7 @@ import "element-plus/theme-chalk/dark/css-vars.css";
 import "photoswipe/style.css";
 import "@fontsource-variable/inter/standard.css";
 import "@fontsource/jetbrains-mono";
+import "./styles/harmonyos-emoji.css";
 import "./styles/harmonyos-sans.css";
 import "./styles/index.scss";
 import "./styles/image-viewer.scss";
