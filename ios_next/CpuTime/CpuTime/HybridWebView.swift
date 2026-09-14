96b75b2fa6deadf9c006aa93149db5a4cdf5d9c8
frank zhang <frankzhang@192.168.10.22>
2026-09-15 05:09:57 +0800

    release: ship iOS 3.12

--- ios_next/CpuTime/CpuTime/HybridWebView.swift
+++ ios_next/CpuTime/CpuTime/HybridWebView.swift
@@ -7,8 +7,8 @@ import WebKit
 
 
 enum IOSNextWebConfiguration {
-    static let versionCode = 28
-    static let versionName = "3.11.0"
+    static let versionCode = 29
+    static let versionName = "3.12.0"
 
     static var appURL: URL {
         let configured = Bundle.main.object(forInfoDictionaryKey: "CPUAppURL") as? String
